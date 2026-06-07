af91a8564a4e7fabb67728beb241aa306c73907c
jager5959 <xuanjie5959@gmail.com>
2026-06-07 22:41:27 +0800

    Delete Stock-Data-Cleaning-Lab directory

--- Stock-Data-Cleaning-Lab/try.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-stock = pd.read_csv("data/raw/dirty_stock_daily_ohlcv.csv")
-stock.head()
-expected_columns={"ticker","date","open","close","high","low","volume"}
-actual_columns = set(stock.columns)
-unexpected_columns = actual_columns-expected_columns
-missing_columns = expected_columns-actual_columns
